cc09fdc8e15d0b2ac2ba165813ddf16c3623d3ab
Zac Todd <zactodd0@gmail.com>
2020-05-20 00:40:59 +1200

    mv hexes

--- colonist_ql/game_structure/structures.py
+++ colonist_ql/game_structure/structures.py
@@ -5,6 +5,17 @@ import colonist_ql.facts as facts
 from collections import defaultdict
 
 
+class Hex:
+    def __init__(self, cube_coords, real_coords, resource, value, blocked=False):
+        self.cube_coords = cube_coords
+        self.real_coords = real_coords
+        self.resource = resource
+        self.value = value
+        self.edges = {frozenset({cube_coords, n}) for n in cc.neighbours(cube_coords)}
+        self.vertices = cc.triples(cube_coords)
+        self.blocked = blocked
+
+
 class Road:
     def __init__(self, edge, dummy=False):
         self.edge = edge
