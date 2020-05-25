054f7100ce42d0c960e6d28d4f8a5f55feff06dc
Zac Todd <zactodd0@gmail.com>
2020-05-25 18:38:39 +1200

    mv game images

--- colonist_ql/facts.py
+++ colonist_ql/facts.py
@@ -1,5 +1,7 @@
 import functools
 import operator
+import colonist_ql.game_images as game_images
+import os
 from enum import Enum
 
 
@@ -61,7 +63,7 @@ RESOURCE_COLOURS = {
     RESOURCES.WOOL: "lightgreen",
     RESOURCES.GRAIN: "gold",
     RESOURCES.ORE: "slategrey",
-    RESOURCES.BRICK: "orange",
+    RESOURCES.BRICK: "orange"
 }
 
 # Board Information
@@ -111,4 +113,13 @@ PORT_PLACEMENT = {
 DICE_PIPS = {2: 1, 3: 2, 4: 3, 5: 4, 6: 5, 7: 6, 8: 5, 9: 4, 10: 3, 11: 2, 12: 1}
 PREPARED_DICE_DIST = functools.reduce(operator.iconcat, [[k] * v for k, v in DICE_PIPS.items()], [])
 
+# Game Images directories
+GAME_IMAGE_DIR = game_images.__path__[0]
 
+PORT_IMAGE_DIR = f"{GAME_IMAGE_DIR}/icons"
+SETTLEMENT_IMAGES_DIR = f"{GAME_IMAGE_DIR}/settlements"
+CITY_IMAGES_DIR = f"{GAME_IMAGE_DIR}/cities"
+ROAD_IMAGES_DIR = f"{GAME_IMAGE_DIR}/road"
+
+
+print(RESOURCE_COLOURS[RESOURCES.WOOL])
