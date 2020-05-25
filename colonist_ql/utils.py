c31aa821432cb48e3a5b9d50d46cb3dc920ad45e
Zac Todd <zactodd0@gmail.com>
2020-05-25 18:40:28 +1200

    bb

--- colonist_ql/utils.py
+++ colonist_ql/utils.py
@@ -1,9 +1,21 @@
 import numpy as np
 import time
+import cv2
 
 
 def distance(a, b):
     return np.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
+
+
+def contour_bounding_box(image, contour):
+    """
+    Gets a sub image base on a contour.
+    :param image: THe image to obtain the sub image from.
+    :param contour: The contour to base the sub image on.
+    :return: The sub image.
+    """
+    x, y, w, h = cv2.boundingRect(contour)
+    return image[y:y + h, x:x + w]
 
 
 def calculate_run_time(func):
